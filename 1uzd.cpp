623ff92a748bc7593c55e2c181a0e0369da39a7e
DziugasD <dapkeviciusdziugas@gmail.com>
2024-02-08 22:17:25 +0200

    medianos skaiciavimas

--- 1uzd.cpp
+++ 1uzd.cpp
@@ -6,7 +6,7 @@ struct studentas{
 	string vardas, pavarde;
 	int skaicius;
 	int pazymiai[10000], egzaminas;
-	double galutinis;
+	double galutinis, mediana;
 };
 
 
@@ -26,11 +26,21 @@ int main(){
 		cin >> v[i].egzaminas;
 		int sum = accumulate(v[i].pazymiai, v[i].pazymiai + v[i].skaicius, 0);
 		v[i].galutinis = 0.4 * sum/v[i].skaicius + 0.6 * v[i].egzaminas;
+
+        sort(v[i].pazymiai, v[i].pazymiai + v[i].skaicius);
+        int vid = v[i].skaicius/2;
+        if (v[i].skaicius%2){
+            v[i].mediana = v[i].pazymiai[vid];
+        }
+        else{
+              v[i].mediana = (v[i].pazymiai[vid] + v[i].pazymiai[vid - 1]) / 2.0;
+        }
 	}
 
 
+
 	for (int i=0; i<n; i++){
-		cout << v[i].vardas << " " << v[i].pavarde << " " << v[i].galutinis << "\n";
+		cout << v[i].vardas << " " << v[i].pavarde << " " << v[i].galutinis << " " << v[i].mediana <<"\n";
 	}
 
 
