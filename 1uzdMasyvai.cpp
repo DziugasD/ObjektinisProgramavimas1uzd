ca2468e0db564d0726adcfdc326b3c48f6121ec5
DziugasD <dapkeviciusdziugas@gmail.com>
2024-02-11 16:34:52 +0200

    c masyvai

--- 1uzdMasyvai.cpp
+++ 1uzdMasyvai.cpp
@@ -4,45 +4,102 @@ using namespace std;
 
 struct studentas{
 	string vardas, pavarde;
-	int skaicius;
-	int pazymiai[10000], egzaminas;
+	int dydis, allocated;
+	int* pazymiai;
+	int egzaminas;
 	double galutinis, mediana;
 };
 
 
 int main(){
 
-	int n;
-	cin >> n;
-	studentas v[n];
+    int dydis = 0, allocated = 1;
+	studentas* v = new studentas[allocated];
 
-	for(int i=0; i<n; i++){
-		cin >> v[i].vardas >> v[i].pavarde >> v[i].skaicius;
-		for(int j=0; j<v[i].skaicius; j++){
-			cin >> v[i].pazymiai[j];
+	while(true){
+        studentas s;
+        cout << "Irasykite studento varda arba noredami baigti iveskite bet koki skaiciu\n";
+        cin >> s.vardas;
+        if(isdigit(s.vardas[0])){
+            break;
+        }
+        cout << "Irasykite studento pavarde\n";
+        cin >> s.pavarde;
+
+        s.dydis = 0, s.allocated = 1, s.pazymiai = new int[s.allocated];
+
+        while(true){
+            int x;
+            cout << "Irasykite studento pazymi arba noredami uzbaigti iveskite bet kokia raide\n";
+            if(!(cin >> x)){
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                break;
+            }
+            if(s.dydis==s.allocated){
+                s.allocated *= 2;
+                int* naujas = new int[s.allocated];
+                for(int i=0; i<s.dydis; i++){
+                    naujas[i] = s.pazymiai[i];
+                }
+                delete[] s.pazymiai;
+                s.pazymiai = naujas;
+            }
+            s.pazymiai[s.dydis] = x;
+            s.dydis++;
+        }
+        cout << "Irasykite studento egzamino pazymi\n";
+		cin >> s.egzaminas;
+
+
+        for(int i=0; i<s.dydis; i++){
+            cout << s.pazymiai[i];
+        }
+
+		int sum = accumulate(s.pazymiai, s.pazymiai + s.dydis, 0);
+
+		if(s.dydis){
+            s.galutinis = 0.4 * sum/s.dydis + 0.6 * s.egzaminas;
+            sort(s.pazymiai, s.pazymiai + s.dydis);
+            int vid = s.dydis/2;
+            if (s.dydis%2){
+                s.mediana = s.pazymiai[vid];
+            }
+            else{
+                  s.mediana = (s.pazymiai[vid] + s.pazymiai[vid - 1]) / 2.0;
+            }
 		}
-		cin >> v[i].egzaminas;
-		int sum = accumulate(v[i].pazymiai, v[i].pazymiai + v[i].skaicius, 0);
-		v[i].galutinis = 0.4 * sum/v[i].skaicius + 0.6 * v[i].egzaminas;
-
-        sort(v[i].pazymiai, v[i].pazymiai + v[i].skaicius);
-        int vid = v[i].skaicius/2;
-        if (v[i].skaicius%2){
-            v[i].mediana = v[i].pazymiai[vid];
-        }
         else{
-              v[i].mediana = (v[i].pazymiai[vid] + v[i].pazymiai[vid - 1]) / 2.0;
+            s.galutinis = 0.6*s.egzaminas;
+            s.mediana = 0;
         }
+
+        if(dydis==allocated){
+            allocated *= 2;
+            studentas* newArr = new studentas[allocated];
+            for(int i=0; i<dydis; i++){
+                newArr[i] = v[i];
+            }
+            delete[] v;
+            v = newArr;
+        }
+
+        v[dydis] = s;
+        dydis++;
 	}
 
-    cout << left << setw(15)  << "Pavardė" << setw(10) << "Vardas" << setw(17) << "Galutinis (Vid.) " << setw(20) << "\\ Galutinis (Med.)\n";
+	//Spausdinimas
+    cout << left << setw(15)  << "Pavarde" << setw(10) << "Vardas" << setw(17) << "Galutinis (Vid.) " << setw(20) << "\\ Galutinis (Med.)\n";
     int bruksneliai = 55;
     while(bruksneliai--) cout << '-';
     cout << "\n";
 
-	for (int i=0; i<n; i++){
+	for (int i=0; i<dydis; i++){
 		cout << fixed << setprecision(2) << setw(15) << v[i].pavarde << setw(10) << v[i].vardas << setw(19) << v[i].galutinis << setw(20) << v[i].mediana <<"\n";
 	}
+
+    for(int i=0; i<dydis; i++) delete [] v[i].pazymiai;
+    delete[] v;
 
 	return 0;
 }
