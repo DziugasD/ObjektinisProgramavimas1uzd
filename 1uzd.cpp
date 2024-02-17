d2a7b87798331cf2fa0d0e30d2c1eb3ef5bb9381
DziugasD <dapkeviciusdziugas@gmail.com>
2024-02-17 19:08:59 +0200

    tikrinimas

--- 1uzd.cpp
+++ 1uzd.cpp
@@ -9,30 +9,82 @@ struct studentas{
 	double galutinis, mediana;
 };
 
+bool yraint(string s){
+    for(int i=0; i<s.size(); i++) if(isdigit(s[i])) return true;
+    return false;
+}
+
 
 int main(){
 
 	int n;
-	cin >> n;
+	cout << "Iveskite studentu skaiciu\n";
+	while(!(cin >> n) || n<1 )
+    {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << "Netinkamas formatas\n";
+    }
 	studentas v[n];
 
 	for(int i=0; i<n; i++){
-		cin >> v[i].vardas >> v[i].pavarde >> v[i].skaicius;
+        cout << "Iveskite studento varda\n";
+        while(!(cin >> v[i].vardas) || yraint(v[i].vardas) )
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Netinkamas formatas\n";
+        }
+        cout << "Iveskite studento pavarde\n";
+        while(!(cin >> v[i].pavarde) || yraint(v[i].pavarde) )
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Netinkamas formatas\n";
+        }
+        cout << "Iveskite studento pazymiu skaiciu\n";
+        while(!(cin >> v[i].skaicius) || v[i].skaicius<0)
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Netinkamas formatas\n";
+        }
 		for(int j=0; j<v[i].skaicius; j++){
-			cin >> v[i].pazymiai[j];
+            cout << "Iveskite studento namu darbu pazymi\n";
+            while(!(cin >> v[i].pazymiai[j]) || v[i].pazymiai[j] < 1 || v[i].pazymiai[j] > 10)
+            {
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cout << "Netinkamas formatas\n";
+            }
 		}
-		cin >> v[i].egzaminas;
-		int sum = accumulate(v[i].pazymiai, v[i].pazymiai + v[i].skaicius, 0);
-		v[i].galutinis = 0.4 * sum/v[i].skaicius + 0.6 * v[i].egzaminas;
+        cout << "Iveskite studento egzamino pazymi\n";
+        while(!(cin >> v[i].egzaminas) || v[i].egzaminas < 1 && v[i].egzaminas > 10)
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Netinkamas formatas\n";
+        }
 
-        sort(v[i].pazymiai, v[i].pazymiai + v[i].skaicius);
-        int vid = v[i].skaicius/2;
-        if (v[i].skaicius%2){
-            v[i].mediana = v[i].pazymiai[vid];
+        if(v[i].skaicius == 0 ){
+            v[i].galutinis = 0.6 * v[i].egzaminas;
+            v[i].mediana = 0.6 * v[i].egzaminas;
         }
         else{
-              v[i].mediana = (v[i].pazymiai[vid] + v[i].pazymiai[vid - 1]) / 2.0;
+            int sum = accumulate(v[i].pazymiai, v[i].pazymiai + v[i].skaicius, 0);
+            v[i].galutinis = 0.4 * sum/v[i].skaicius + 0.6 * v[i].egzaminas;
+
+            sort(v[i].pazymiai, v[i].pazymiai + v[i].skaicius);
+            int vid = v[i].skaicius/2;
+            if (v[i].skaicius%2){
+                v[i].mediana = v[i].pazymiai[vid];
+            }
+            else{
+                  v[i].mediana = (v[i].pazymiai[vid] + v[i].pazymiai[vid - 1]) / 2.0;
+            }
+            v[i].mediana = 0.4 * v[i].mediana + 0.6 * v[i].egzaminas;
         }
+
 	}
 
     cout << left << setw(15)  << "Pavardė" << setw(10) << "Vardas" << setw(17) << "Galutinis (Vid.) " << setw(20) << "\\ Galutinis (Med.)\n";
